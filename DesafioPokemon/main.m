3a782120b01e057722bdd1d6695002f5e38710be
Geovanni de Jesus <geovannioliveiradejesus@Mac315.local>
2017-03-15 17:27:01 -0300

    Initial Commit

---
+++ DesafioPokemon/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DesafioPokemon
+//
+//  Created by Geovanni Oliveira de Jesus on 15/03/17.
+//  Copyright © 2017 Geovanni Oliveira de Jesus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
